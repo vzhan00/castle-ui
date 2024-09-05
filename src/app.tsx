47fd570f3f233fbc59064c0f1a002ed9d32b35a2
cjdoherty1 <cjdoherty883@gmail.com>
2024-09-05 13:20:08 -0400

    Added client side supabase authorization (login/signup)

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+// Filename - App.js
+
+import React, { Component } from "react";
+import SignIn from "./Components/SignIn";
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<SignIn/>
+			</div>
+		);
+	}
+}
+
+export default App;
