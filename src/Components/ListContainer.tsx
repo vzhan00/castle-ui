6fc4e500a53fd6060dfc74eb66b5a92bc0c9a1d0
cjdoherty1 <cjdoherty883@gmail.com>
2024-09-24 15:59:16 -0400

    changed spacing, added buttons, and changede scroll, for list containers

--- src/Components/ListContainer.tsx
+++ src/Components/ListContainer.tsx
@@ -1,32 +1,63 @@
-import { CardContent, Typography } from "@mui/material";
+import { Button, CardContent } from "@mui/material";
 import Card from "@mui/material/Card";
 import Draggable from "react-draggable";
 import { WatchlistProps } from "../types/Watchlist";
 import { PosterGrid } from "./PosterGrid";
+import AddIcon from "@mui/icons-material/Add";
+import { BiExpandAlt } from "react-icons/bi";
+import { IconButton } from "@mui/material";
 
 export function ListContainer({ watchlist }: WatchlistProps) {
     return (
         <Draggable>
-            <div style={{
+            <div
+                style={{
                     textAlign: "center",
                     color: "white",
-                    width: "15vw",
-                    textShadow: '2px 2px 5px black',
-                }}>
+                    width: 500,
+                    textShadow: "2px 2px 5px black",
+                }}
+            >
+                <IconButton sx={{
+                        height: 37,
+                        width: 37,
+                        position: "absolute",
+                        top: 55,
+                        right: 15,
+                        zIndex: 1
+                    }}><AddIcon/></IconButton>
+                    <IconButton sx={{
+                        height: 37,
+                        width: 37,
+                        position: "absolute",
+                        top: 8,
+                        right: 15,
+                        zIndex: 1
+                    }}><BiExpandAlt style={{
+                        fontSize: 20
+                    }}/></IconButton>
                 <Card
                     sx={{
                         backgroundColor: "rgba(255, 255, 255, 0.3)",
-                        width: "15vw",
-                        height: "35vh",
-                        overflow: "auto",
-                        borderRadius: 2
+                        height: 407,
+                        width: 500,
+                        overflowY: "auto",
+                        scrollbarWidth: "thin",
+                        scrollbarColor: "transparent transparent",
+                        borderRadius: 8,
+                        backdropFilter: "blur(5px)",
                     }}
                 >
-                    <CardContent>
-                        <PosterGrid watchlist={watchlist} />
-                    </CardContent>
+                        <CardContent sx={{
+                            padding: 5
+                        }}>
+                            <PosterGrid watchlist={watchlist} />
+                        </CardContent>
                 </Card>
-                <h3 style={{textShadow: '6px 6px 15px rgba(0, 0, 0, 1)'}}>{watchlist.watchlistName}</h3>
+
+                <h3 style={{ textShadow: "6px 6px 15px rgba(0, 0, 0, 1)" }}>
+                    {watchlist.watchlistName}
+                </h3>
             </div>
         </Draggable>
     );
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,29 +4,30 @@ import React from "react";
 
 import { Container } from "@mui/material";
 import Button from "@mui/material/Button";
-import { useLazyGetWatchlistByIdQuery } from "../../services/WatchlistApi";
 import { supabase } from "../../supabase";
-import { WatchlistView } from "../../components/Watchlist";
 import { useRouter } from "next/navigation";
+import { ListContainer } from "../../components/ListContainer";
+import { useGetAllWatchlistsQuery } from "../../services/WatchlistApi";
 
 export default function Home() {
     const router = useRouter();
-    const [trigger, { data }] = useLazyGetWatchlistByIdQuery();
 
-    const onClickWatchlist = () => {
-        trigger('1');
-    }
+    const watchlistsResponse = useGetAllWatchlistsQuery().data;
+    const watchlists = watchlistsResponse?.allWatchlists
+    
+    console.log(watchlists);
 
     const signOut = () => {
+        console.log(5555)
         supabase.auth.signOut();
-        router.push('/login');
     }
 
     return (
         <Container>
-            <Button onClick={onClickWatchlist} variant="contained">Get Watchlist</Button>
             <Button onClick={signOut} variant="contained">Log out</Button>
-            {data && <WatchlistView watchlist={data.watchlist} />}
+            {watchlists?.map((watchlist) => (
+                <ListContainer watchlist={watchlist}/>
+            ))}
         </Container>
-    )
+    );
 }
