7ac87fa63c56e14324ca68ebb67c665bf47eb07a
cjdoherty1 <cjdoherty883@gmail.com>
2024-11-16 13:17:05 -0500

    Added settings and addwatchlist buttons in task bar

---
+++ src/Components/AddWatchlistButton.tsx
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import SettingsIcon from "@mui/icons-material/Settings";
+import GridViewIcon from '@mui/icons-material/GridView';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import WallpaperIcon from '@mui/icons-material/Wallpaper';
+import ScheduleIcon from '@mui/icons-material/Schedule';
+import CheckIcon from '@mui/icons-material/Check';
+import AddIcon from '@mui/icons-material/Add';
+
+export default function AddWatchlistButton() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <React.Fragment>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <AddIcon/>
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        slotProps={{
+          paper: {
+            elevation: 0,
+            sx: {
+              overflow: 'visible',
+              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+              mt: 1.5,
+              '& .MuiAvatar-root': {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              '&::before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: 'background.paper',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
+              },
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <ScheduleIcon fontSize="small" />
+          </ListItemIcon>
+          Add Watchlist
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <CheckIcon fontSize="small" />
+          </ListItemIcon>
+          Add Watched List
+        </MenuItem>
+      </Menu>
+    </React.Fragment>
+  );
+}
--- src/Components/Appbar.tsx
+++ src/Components/Appbar.tsx
@@ -1,40 +1,52 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import { supabase } from '../supabase';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import { supabase } from "../supabase";
 import { useRouter } from "next/navigation";
-import CastleIcon from '@mui/icons-material/Castle';
-import LogoutIcon from '@mui/icons-material/Logout';
-import SettingsIcon from '@mui/icons-material/Settings';
+import CastleIcon from "@mui/icons-material/Castle";
+import LogoutIcon from "@mui/icons-material/Logout";
+import SettingsIcon from "@mui/icons-material/Settings";
+import SettingsButton from "./SettingsButton";
+import AddWatchlistButton from "./AddWatchlistButton";
 
 export default function HomeAppBar() {
     const router = useRouter();
+    
     const signOut = () => {
         supabase.auth.signOut();
         router.push("/login");
     };
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{
-        backgroundColor: "rgba(0,0,0,0)",
-        boxShadow: "none"
-      }}>
-        <Toolbar sx={{height: 25, padding: 0, margin: 0, color: "#324352"}}>
-          <CastleIcon sx={{mr: 2}}/>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          </Typography>
-          <IconButton>
-            <SettingsIcon/>
-          </IconButton>
-          <IconButton onClick={signOut}>
-            <LogoutIcon/>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar
+                position="static"
+                sx={{
+                    backgroundColor: "rgba(0,0,0,0)",
+                    boxShadow: "none",
+                }}
+            >
+                <Toolbar
+                    sx={{ height: 25, padding: 0, margin: 0, color: "#324352" }}
+                >
+                    <CastleIcon sx={{ mr: 2 }} />
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    ></Typography>
+                    <AddWatchlistButton/>
+                    <SettingsButton/>
+                    <IconButton onClick={signOut} sx={{paddingLeft: "20px"}}>
+                        <LogoutIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
